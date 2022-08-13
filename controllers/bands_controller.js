faad262d5e0decb8f3d3c86395b5cc0031d553e0
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-08-13 12:36:25 -0400

    routes

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -12,5 +12,65 @@ bands.get('/', async (req, res) => {
         res.status(500).json(error)
     }
 })
+
+//find a specific band
+bands.get('/:id', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { band_id: req.params.id }
+        })
+        res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// create a band
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// update a band
+bands.put('/:id', async (req, res) => {
+    try {
+        const updatedBands = await Band.update(req.body, {
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedBands} band(s)`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// delete a band
+bands.delete('/:id', async (req, res) => {
+    try {
+        const deletedBands = await Band.destroy({
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedBands} band(s)`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
 //export
 module.exports = bands
