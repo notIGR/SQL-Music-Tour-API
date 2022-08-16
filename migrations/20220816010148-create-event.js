11c3d316371f5e63c0478e6f23e6820de52b630b
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-08-15 21:05:20 -0400

    sequelize

---
+++ migrations/20220816010148-create-event.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      event_id: {
+        type: Sequelize.INTEGER
+      },
+      end_time: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Events');
+  }
+};
--- models/stages.js
+++ models/stages.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Event extends Model {
+  class Stages extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,12 +13,12 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Event.init({
+  Stages.init({
     event_id: DataTypes.INTEGER,
     end_time: DataTypes.DATE
   }, {
     sequelize,
-    modelName: 'Event',
+    modelName: 'Stages',
   });
-  return Event;
+  return Stages;
 };
