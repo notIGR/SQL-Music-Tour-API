c017990991667d9552d90cdc57055d3e4d307bda
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-08-15 20:30:20 -0400

    added controlles route to server.js file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const { Sequelize } = require('sequelize')
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+const bands = require('./controllers/bands_controller')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
@@ -20,6 +21,12 @@ app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
 
-//controllers
+// controllers
 const bandsController = require('./controllers/bands_controller')
 app.use('/bands', bandsController)
+
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
